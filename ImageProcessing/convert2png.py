31813c52d05341909903735af4a8d38bebb3fe60
kojiryo <36512360+kojiryo@users.noreply.github.com>
2019-02-09 01:59:39 +0900

    Update convert2png.py

--- ImageProcessing/convert2png.py
+++ ImageProcessing/convert2png.py
@@ -13,10 +13,17 @@ def main():
         print('usage: convert2png.py <path/to/*.ppm> ...')
         sys.exit(1)
 
+    os.makedirs('result/convert2png', exist_ok=True)
+
     for i in range(1, argc):
         img = cv2.imread(argvs[i])
+
+        # root, ext = os.path.splitext(argvs[i])
+        # cv2.imwrite(root + '.png', img)
+
         root, ext = os.path.splitext(argvs[i])
-        cv2.imwrite(root + '.png', img)
+        strImgName = root.split('/')[-1]
+        cv2.imwrite('result/convert2png/' + strImgName + '.png', img)
 
 
 if __name__ == '__main__':
