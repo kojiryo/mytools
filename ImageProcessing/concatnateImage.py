8d77bbfac355cb0101ff73c469e9da04c3c4a6ce
kojiryo <36512360+kojiryo@users.noreply.github.com>
2019-02-09 01:59:15 +0900

    Update concatnateImage.py

--- ImageProcessing/concatnateImage.py
+++ ImageProcessing/concatnateImage.py
@@ -1,7 +1,6 @@
 import cv2
 import sys
-# import os
-# import numpy as np
+import os
 
 
 def main():
@@ -14,6 +13,8 @@ def main():
         print('usage: concatnateImage.py <path/to/*.png> ...')
         sys.exit(1)
 
+    os.makedirs('result/concatnateImage', exist_ok=True)
+
     im_list = []
     for i in range(1, argc):
         im_list.append(cv2.imread(argvs[i]))
@@ -22,9 +23,13 @@ def main():
 
     im_list_resize = [cv2.resize(
         im, (w_min, int(im.shape[0] * w_min / im.shape[1]))) for im in im_list]
+    img = cv2.vconcat(im_list_resize)
 
-    outputimg = cv2.vconcat(im_list_resize)
-    cv2.imwrite('output.png', outputimg)
+    # cv2.imwrite('concatImage.png', img)
+
+    strImgName = argvs[1].split('/')[-1]
+    root, ext = os.path.splitext(strImgName)
+    cv2.imwrite('result/concatnateImage/Concat_' + root + '.png', img)
 
 
 if __name__ == '__main__':
