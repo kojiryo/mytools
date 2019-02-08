c92b86f1610f84db54f78795a9519f2b8880c56a
kojiryo <36512360+kojiryo@users.noreply.github.com>
2019-02-09 02:00:54 +0900

    Update resizeImageByHeight.py

--- ImageProcessing/resizeImageByHeight.py
+++ ImageProcessing/resizeImageByHeight.py
@@ -9,18 +9,25 @@ def main():
     """
     argvs = sys.argv
     argc = len(argvs)
-    if argc <= 3:
+    if argc <= 2:
         print(
             'usage: resizeImageByHeight.py <height[pixel]> <path/to/*.png> ...')
         sys.exit(1)
 
     height = int(argvs[1])
 
+    os.makedirs('result/resizeImageByHeight', exist_ok=True)
+
     for i in range(2, argc):
         im = cv2.imread(argvs[i])
         img = cv2.resize(im, (int(im.shape[1] * height / im.shape[0]), height))
-        root, ext = os.path.splitext(argvs[i])
-        cv2.imwrite(root + '.png', img)
+
+        # root, ext = os.path.splitext(argvs[i])
+        # cv2.imwrite(root + '_resize.png', img)
+
+        strImgName = argvs[i].split('/')[-1]
+        cv2.imwrite('result/resizeImageByHeight/H'
+                    + str(height) + '_' + strImgName, img)
 
 
 if __name__ == '__main__':
